2685cfa0283b81c8e9b24654969b4569cc77627e
Lem Canady <lem.canady@gmail.com>
2023-12-31 14:33:27 -0800

    removing console.log

--- mod.ts
+++ mod.ts
@@ -1,6 +1,5 @@
 import { __dirname, join, plugins } from "./deps.ts";
 
 export default () => {
-  console.log("Loading Commands: ", join(__dirname, "./commands"));
   plugins(join(__dirname, "./commands"));
 };
