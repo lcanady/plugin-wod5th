9ef2df2dfcb48fb391f0eeadea37cbd1a0a34d4d
Lem Canady <lem.canady@gmail.com>
2024-02-03 22:43:24 -0800

    testing

--- mod.ts
+++ mod.ts
@@ -1,6 +1,7 @@
 import { __dirname, join, loadTxtDir, plugins } from "./deps.ts";
 
 export default () => {
+  console.log("Loading...");
   loadTxtDir(join(__dirname, "./text"));
   plugins(join(__dirname, "./plugins"));
   plugins(join(__dirname, "./commands"));
